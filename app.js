75eca02ef2f2fd22f0d53c09c2d7b50f4ead6eba
Nazari <n.adamenko@tg.dunice.net>
2023-07-18 00:29:26 +0300

    [node_news-site_001] - долена ортка приоения и синхронизация бд при старте сервера

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-
+const db = require('./models/index');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+
+const start = async () => {
+  try {
+    await db.sequelize.authenticate();
+    await db.sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
